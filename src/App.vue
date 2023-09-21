c51ace6da201c23184ab0a662c4dbfbd9e97c06e
amandine lescieux (ext) <amandine.lescieux@FR-25B2ML3.groupinfra.com>
2023-09-21 14:41:26 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import RickAndMortyCards from "@/components/RickAndMortyCards.vue";
+</script>
+
+<template>
+  <main>
+    <h1>HERO</h1>
+    <RickAndMortyCards />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/RickAndMortyCards.vue
@@ -0,0 +1,16 @@
+<script setup>
+import axios from "axios";
+
+const response = await axios.get('https://rickandmortyapi.com/api/character')
+
+</script>
+
+<template>
+  <div>
+    <h1>Rick & Morty Cards</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
